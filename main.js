25fffc30dbf63e38663417044cf57f1f9eec2fa4
davit abakelia <abakelia_dato@mail.ru>
2024-04-22 01:03:01 +0300

    add loading page

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
-'use strict';
 
+'use strict';
 
 
 /**
  * PRELOAD
  * 
- * loading will be end after document is loaded
+ * loading will be end after cocument is loaded
  */
 
 const preloader = document.querySelector("[data-preaload]");
 
 window.addEventListener("load", function () {
-  preloader.classList.add("loaded");
   document.body.classList.add("loaded");
 });
 
-
-
-/**
- * add event listener on multiple elements
- */
-
-const addEventOnElements = function (elements, eventType, callback) {
-  for (let i = 0, len = elements.length; i < len; i++) {
-    elements[i].addEventListener(eventType, callback);
+gsap.fromTo(
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 3.5,
   }
-}
+);
 
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0.5,
+  }
+);
